a8ecd444c85af0ba38a21ba867062c90cfa652be
Trivini <sakshis8767@gmail.com>
2025-09-25 10:57:07 +0530

    LMS with Array

---
+++ Arrays/LMS.java
@@ -0,0 +1,41 @@
+package Arrays;
+
+ 
+//package Constructors;
+
+public class LMS {
+
+    public static void main(String args[]){
+
+        Student s1 = new Student(101 , "Sakshi");
+
+        Student s2 = new Student(201 , "Shinde", 989898, 23);
+
+        //Hover Functionality
+        s1.StudentInfo();
+
+        //Click functionality
+        s1.StudentCompleteInfo();
+
+        //display session credits
+        //System.out.println(s1.sessionsCredits());
+       // System.out.println(s2.sessionsCredits());
+       
+       s1.achievementStatus();
+
+        // trainer 
+        Trainer t1 = new Trainer();
+        t1.trainerID = 101;
+        t1.trainerName = "John";
+
+        // info of trainer
+        t1.trainerInfo();
+
+        // trainer payment
+        t1.sessionsPayment();
+
+    }
+
+    
+}
+
