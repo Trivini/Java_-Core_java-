99a6e1bb758db47ce2b27eafb814b0e77ec7caa0
Trivini <sakshis8767@gmail.com>
2025-09-29 17:34:46 +0530

    LMS with Strings

---
+++ Strings/LMS.java
@@ -0,0 +1,44 @@
+package Strings;
+
+ 
+//package Constructors;
+
+public class LMS {
+
+    public static void main(String args[]){
+
+        Student s1 = new Student(101 , "Sakshi");
+
+        Student s2 = new Student(201 , "Shinde", 989898, 23, 50000, "FIFTY");
+
+        //Hover Functionality
+        s1.StudentInfo();
+
+        //Click functionality
+        s1.StudentCompleteInfo();
+        s1.calculatecourseFees();
+
+        //System.out.println(s1.sessionsCredits());
+       // System.out.println(s2.sessionsCredits());
+       //display session credits
+       s1.achievementStatus();
+
+       s2.StudentCompleteInfo();
+       s2.calculatecourseFees();
+
+        // trainer 
+        Trainer t1 = new Trainer();
+        t1.trainerID = 101;
+        t1.trainerName = "John";
+
+        // info of trainer
+        t1.trainerInfo();
+
+        // trainer payment
+        t1.sessionsPayment();
+
+    }
+
+    
+}
+
