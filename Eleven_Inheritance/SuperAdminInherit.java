67a4e5a72e3d9ca2c5dbbe6832ba6ba1364c5191
Trivini <sakshis8767@gmail.com>
2025-10-09 21:04:30 +0530

    With Inheritanve

---
+++ Eleven_Inheritance/SuperAdminInherit.java
@@ -0,0 +1,29 @@
+package Eleven_Inheritance;
+
+public class SuperAdminInherit extends VideoAdminInherit {
+
+    //watch video and add video coming from VedioAdminInherit
+
+    //Delete Video
+    
+
+    public void DeleteVideo(){
+        System.out.println("D");
+        System.out.println("E");
+        System.out.println("L");
+        System.out.println("E");
+        System.out.println("T");
+        System.out.println("E");
+        System.out.println("V");
+        System.out.println("I");
+        System.out.println("D");
+        System.out.println("E");
+        System.out.println("O");
+        System.out.println(".");
+        System.out.println(".");
+        System.out.println(".");
+
+
+    }
+    
+}
