2ba86b1914bbd42eca5aea21ac55950deaeb69ac
Trivini <sakshis8767@gmail.com>
2025-10-10 00:25:42 +0530

    super Keyword

---
+++ Eleven_Inheritance/Animal.java
@@ -0,0 +1,10 @@
+package Eleven_Inheritance;
+
+public class Animal {
+    
+    String animal = " Super Animal";
+
+    public void display(){
+        System.out.println("Super Animal Displayed");
+    }
+}
