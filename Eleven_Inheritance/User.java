e180ccc8033bd23721d81057161cbbdbf42f378c
Trivini <sakshis8767@gmail.com>
2025-10-09 21:04:36 +0530

    With Inheritanve

--- Eleven_Inheritance/User.java
+++ Eleven_Inheritance/User.java
@@ -4,7 +4,7 @@ public class User {
 
     public static void main(String[] args) {
 
-         System.out.println("=====Learner======");
+        System.out.println("=====Learner======");
         Learner learner = new Learner();
         learner.watchVideo();
 
@@ -20,6 +20,28 @@ public class User {
         superadmin.watchVideo();
         superadmin.UpdateVideo();
         superadmin.DeleteVideo();
+
+        System.out.println("===== With Inheritance ======");
+
+        System.out.println("=====Learner======");
+
+        //Learner learner = new Learner();
+        learner.watchVideo();
+
+        System.out.println("===== Video Admin======");
+        
+        VideoAdminInherit videoAdminInherit = new VideoAdminInherit();
+        videoAdminInherit.watchVideo();
+        videoAdminInherit.UpdateVideo();
+
+        System.out.println("===== Super Admin======");
+
+        SuperAdminInherit superadminInherit = new SuperAdminInherit();
+        superadminInherit.watchVideo();
+        superadminInherit.UpdateVideo();
+        superadminInherit.DeleteVideo();
+
+        
     }
     
 }
