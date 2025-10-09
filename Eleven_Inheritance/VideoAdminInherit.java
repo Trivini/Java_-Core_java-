af2fcfb6a1fe3c1c8b14f2afbfc5dced3e073d25
Trivini <sakshis8767@gmail.com>
2025-10-09 21:04:42 +0530

    With Inheritanve

---
+++ Eleven_Inheritance/VideoAdminInherit.java
@@ -0,0 +1,27 @@
+package Eleven_Inheritance;
+
+public class VideoAdminInherit extends Learner {
+    //video admin can watch vedios update the vedios but not delete vedios
+
+    //watch video coming from Learner
+
+    public void UpdateVideo(){
+        System.out.println("U");
+        System.out.println("P");
+        System.out.println("D");
+        System.out.println("A");
+        System.out.println("T");
+        System.out.println("E");
+        System.out.println("V");
+        System.out.println("I");
+        System.out.println("D");
+        System.out.println("E");
+        System.out.println("O");
+        System.out.println(".");
+        System.out.println(".");
+        System.out.println(".");
+
+
+    }
+    
+}
