dbb41937ac85e60f3dc8099e7eed57fc743ca548
Trivini <sakshis8767@gmail.com>
2025-10-11 12:17:46 +0530

    abstraction and interface

---
+++ thirteen_Abstraction/Test.java
@@ -0,0 +1,14 @@
+package thirteen_Abstraction;
+
+public class Test {
+
+   
+    //concrete method
+    public void show(){
+        System.out.println("Laptop");
+    }
+
+    //abstract method
+   // public void Newshow();
+    
+}
