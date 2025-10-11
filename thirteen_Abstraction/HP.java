732508357d0b2e0c273ff16c344bc258e2571b25
Trivini <sakshis8767@gmail.com>
2025-10-11 16:39:59 +0530

    Abstarct Class

---
+++ thirteen_Abstraction/HP.java
@@ -0,0 +1,12 @@
+package thirteen_Abstraction;
+
+public abstract class HP implements NewLaptop{
+
+    public void m1(){
+        System.out.println("m1 is Implementing...");
+        
+    }
+
+    public abstract void m4();
+
+}
