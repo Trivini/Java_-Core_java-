6c70af3db674c2d03986beea07f50c6e6b47d427
Trivini <sakshis8767@gmail.com>
2025-10-11 16:16:39 +0530

    Type Casting

---
+++ thirteen_Abstraction/TypeConversion.java
@@ -0,0 +1,15 @@
+package thirteen_Abstraction;
+
+public class TypeConversion {
+    public static void main(String[] args) {
+        double num = 3.14;
+        //int new_num = num;
+
+        int new_num = (int) num;
+        System.out.println(num);
+        System.out.println(new_num);
+    }
+
+    
+    
+}
