d25048577aee5d8ac7900b791a468e1f32a38caa
Trivini <sakshis8767@gmail.com>
2025-10-13 20:40:31 +0530

    LMS with abstraction

---
+++ Fourteen_Polymorphism/Payable.java
@@ -0,0 +1,14 @@
+package Fourteen_Polymorphism;
+
+//calulating payment for student, trainers etc
+
+public interface Payable {
+
+    //contract
+    public void calculatePayment();
+
+   // void generateReceipt(String transactionId);
+
+    
+
+}
