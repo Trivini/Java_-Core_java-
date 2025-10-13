04f7cc346299b9aae408446d54900c57dca72a5d
Trivini <sakshis8767@gmail.com>
2025-10-13 20:40:09 +0530

    LMS with abstraction

---
+++ Fourteen_Polymorphism/AbstractPerson.java
@@ -0,0 +1,63 @@
+package Fourteen_Polymorphism;
+
+public abstract class AbstractPerson {
+
+    //all common attributes
+    private int personID;
+    private String personName;
+    private int personAge;
+    private int personMobileNumber;
+
+   
+
+    public int getPersonID() {
+        return personID;
+    }
+
+
+
+    public void setPersonID(int personID) {
+        this.personID = personID;
+    }
+
+
+
+    public String getPersonName() {
+        return personName;
+    }
+
+
+
+    public void setPersonName(String personName) {
+        this.personName = personName;
+    }
+
+
+
+    public int getPersonAge() {
+        return personAge;
+    }
+
+
+
+    public void setPersonAge(int personAge) {
+        if(personAge>0){
+            this.personAge = personAge;
+        }else{
+            System.out.println("Age is above 0");
+        }
+    }
+
+
+
+    public int getPersonMobileNumber() {
+        return personMobileNumber;
+    }
+
+
+
+    public void setPersonMobileNumber(int personMobileNumber) {
+        this.personMobileNumber = personMobileNumber;
+    }
+
+}
